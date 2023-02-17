2d630eff9ce333a2b8b68411455a85d4a67b2590
sheinz20 <sheinz@norwich.edu>
2023-02-17 16:53:46 -0500

    Initial Commit

---
+++ myProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myProject
+//
+//  Created by Sophia Heinz on 2/17/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
